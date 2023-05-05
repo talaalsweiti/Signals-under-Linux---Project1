e85d9da0966e5eaddc5bd9f7c1ea07646998b3c6
ShahdDaghash <shahddaghash18@gmail.com>
2023-05-05 23:41:04 +0300

    created range.txt and signal to children

--- child.cpp
+++ child.cpp
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <fstream>
 
-int main(){
-    printf("%d \n", atoi(" 10aseel"));
-    printf("Shahd");
+using namespace std;
+
+void beginSignalCatcher(int);
+
+int main()
+{
+    if (sigset(SIGUSR1, beginSignalCatcher) == SIG_ERR)
+    {
+        perror("Sigset can not set SIGINT");
+        exit(SIGUSR1);
+    }
     return 0;
+}
+
+void beginSignalCatcher(int theSig)
+{
+    // read range
+    // ifstream rangeFile ("range.txt");
+    // if(rangeFile.good()){
+        
+    // }
+    // else{
+    //     exit(2);
+    // }
+    // give a double
+    // write double on file
 }
--- parent.cpp
+++ parent.cpp
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <signal.h>
 #include <unistd.h>
 #include <ctype.h>
 #include <string>
 #include <string.h>
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 unsigned children[5];
 
 
 void createProcesses(char*,int);
+void generateRange();
+void childDeadSignalCatcher(int);
+void cleanup();
 
 int main(int argc, char *argv[])
 {
@@ -37,13 +43,29 @@ int main(int argc, char *argv[])
             exit(3);
         }
     }
-    // printf("rounds=%d\n", rounds);
     for (int i = 0; i < 4; i++)
     {
         createProcesses("./child",i);
     }
-        createProcesses("./coprocessor",4);
+    
+    createProcesses("./coprocessor",4);
 
+    sigset(SIGCHLD, childDeadSignalCatcher);
+
+    srand((unsigned) getpid());
+
+
+    for(int i=0; i<rounds; i++){
+        generateRange();
+        
+        for(int i=0; i<1; i++){
+            kill(children[i], SIGUSR1);
+        }
+        
+        sleep(10);
+    }
+    
+    cleanup();
  
 }
 
@@ -66,3 +88,40 @@ void createProcesses(char* file, int i)
         children[i] = pid;
     }
 }
+
+
+void generateRange(){
+    ofstream rangeFile;
+    rangeFile.open("range.txt");
+
+    int minValue = rand();
+    int maxValue = rand();
+    
+    if(minValue > maxValue){
+        swap(minValue, maxValue);
+    }
+
+    rangeFile << minValue << "," << maxValue;
+}
+
+
+void childDeadSignalCatcher(int theSig){
+    cout << "A child failed\n";
+    cleanup();
+    exit(theSig);
+    
+}
+
+
+void cleanup(){
+    for(auto child: children){
+        kill(child, SIGKILL);
+    }
+    exit(0);
+}
+
+/*
+double range = (max - min); 
+    double div = RAND_MAX / range;
+    return min + (rand() / div);
+*/
