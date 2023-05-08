6e8261d6500dc12010f9c8f162a3862a6616d317
ShahdDaghash <shahddaghash18@gmail.com>
2023-05-08 19:31:15 +0300

    Added comments to child.cpp

--- child.cpp
+++ child.cpp
@@ -4,17 +4,20 @@ using namespace std;
 
 void beginSignalCatcher(int);
 
-// this the the first comment
 int main()
 {
-    cout << "I am Child\n";
-    srand((unsigned)getpid());
+    cout << "I am Child with PID " << getpid() << "\n";
+    
+    srand((unsigned)getpid()); /* Set child PID as unique seed for rand */
 
+    /* set up signal handler when SIGUSR1 is called by the parent */
     if (sigset(SIGUSR1, beginSignalCatcher) == SIG_ERR)
     {
         perror("Sigset can not set SIGUSR1");
         exit(SIGUSR1);
     }
+
+    /* suspend the child process until a signal is called */
     while (1)
     {
         pause();
@@ -23,21 +26,24 @@ int main()
     return 0;
 }
 
-void beginSignalCatcher(int theSig) // generateValue
+/*
+    This function is the SIGUSR1 Signal Handler.
+    It reads the range values and sends back a random generated value between MIN and MAX
+*/
+void beginSignalCatcher(int theSig)
 {
 
-    // read range
+    /* read MIN and MAX from range.txt */
     ifstream rangeFile("/tmp/range.txt");
     if (!rangeFile.good())
     {
         perror("Open range.txt");
         exit(2);
     }
-
     string line;
-
     getline(rangeFile, line);
 
+    /* split string <MIN>,<MAX> into the array rangeValues */
     vector<string> rangeValues(2);
     unsigned int i = 0;
     stringstream sline(line);
@@ -48,14 +54,17 @@ void beginSignalCatcher(int theSig) // generateValue
         getline(sline, substr, ',');
         rangeValues[i++] = substr;
     }
-    // check for errors (empty line ...)
 
+    /* convert MIN and MAX from string to int */
     int minValue = stoi(rangeValues[0]);
     int maxValue = stoi(rangeValues[1]);
-    double range = (maxValue - minValue);
+
+    /* generate random value in range using rand() function */
+    double range = (maxValue - minValue); 
     double div = RAND_MAX / range;
     double value = minValue + (rand() / div);
 
+    /* write the generated value to <PID>.txt file */
     ofstream generatedNumFile;
     string fileName = "/tmp/" + to_string(getpid()) + ".txt";
     generatedNumFile.open(fileName);
@@ -64,5 +73,6 @@ void beginSignalCatcher(int theSig) // generateValue
 
     generatedNumFile.close();
 
+    /* send signal to parent to inform of finishing writing */
     kill(getppid(), SIGUSR1);
 }
